aa31e64560d738dbdab7300405ed47848163e549
Jorgelin <phenomelmetinero@gmail.com>
2016-11-03 23:28:08 -0400

    Busqueda mejorada

--- search.py
+++ search.py
@@ -9,8 +9,14 @@ class Search(Handler):
         search = self.request.get('q')
         filter_search = self.request.get('filter')
         query = None
+        query_all = None
         if filter_search == 'User' or filter_search == 'Post' or filter_search == 'Comment':
             query = list(db.GqlQuery('select * from %s' % str(filter_search)))
+        elif filter_search == 'All':
+            query_all = []
+            query_all.append(list(db.GqlQuery('select * from User')))
+            query_all.append(list(db.GqlQuery('select * from Post')))
+            query_all.append(list(db.GqlQuery('select * from Comment')))
         if query:
             html = []
             for ob in query:
@@ -23,4 +29,18 @@ class Search(Handler):
                 elif filter_search == 'Comment':
                     if ob.content.lower().find(search.lower()) > -1:
                         html = html + [ob]
+        elif query_all:
+            html,html1,html2,html3 = [],[],[],[]
+            for ob in query_all[0]:
+                if ob.displayName.lower().find(search.lower()) > -1:
+                    html1 = html1 + [ob]
+            for ob in query_all[1]:
+                if ob.title.lower().find(search.lower()) > -1:
+                    html2 = html2 + [ob]
+            for ob in query_all[2]:
+                if ob.content.lower().find(search.lower()) > -1:
+                    html3 = html3 + [ob]
+            html.append(html1)
+            html.append(html2)
+            html.append(html3)
         self.render('search.html',pagename='Resultados de busqueda',user=user,data=html,data_type=filter_search,recent_msg=messages,menu='filter',search=search)
